145d9dbd388771d6af3e3a5a498989c03972604c
João Davi Santos <joodavi@live.com>
2021-03-30 09:59:36 -0300

    Update q22.py

--- 02 - Condições/q22.py
+++ 02 - Condições/q22.py
@@ -1,4 +1,6 @@
-# Uma empresa vende o mesmo produto para quatro diferentes estados. Cada estado possui uma taxa de imposto sobre o produto (MG 7%; SP %12; RJ 15%; MS 8%). Faça um programa em que o usuário entre com o valor e o estado de destino do produto e o programa retorne o preço final do produto acrescido do imposto do estado em que ele será vendido. Se o estado digitado não for válido, mostrar uma mensagem de erro.
+# Uma empresa vende o mesmo produto para quatro diferentes estados. Cada estado possui uma taxa de imposto sobre o produto (MG 7%; SP %12; RJ 15%; MS 8%).
+# Faça um programa em que o usuário entre com o valor e o estado de destino do produto e o programa retorne o preço final do produto acrescido do imposto do estado em que 
+# ele será vendido. Se o estado digitado não for válido, mostrar uma mensagem de erro.
 
 print('Informe a sigla referente ao estado(Use maiúsculas)')
 estado = str(input('Digite o estado '))
