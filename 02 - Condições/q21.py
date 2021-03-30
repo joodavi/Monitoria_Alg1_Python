74364fe4a2c46c21330f97eec138a6ad4b259caa
João Davi Santos <joodavi@live.com>
2021-03-30 09:58:58 -0300

    Update q21.py

--- 02 - Condições/q21.py
+++ 02 - Condições/q21.py
@@ -1,4 +1,7 @@
-# A nota final de um estudante é calculada a partir de três noas atribuídas entre o intervalo de 0 até 10, respectivamente, a um trabalho de laborátorio, a uma avaliação semestral e um exame final. A média das três notas mencionadas anteriormente obedece aos pesos: Trabalho de Laboratório: 2; Avaliação Semestral: 3; Exame Final: 5. De acordo com o resultado, mostre na tela se o aluno está aprovado (média entre 0 e 2,9) de recuperação (entre 3 e 4,9) ou se foi aprovado. Faça todas as verificações necessárias.
+# A nota final de um estudante é calculada a partir de três noas atribuídas entre o intervalo de 0 até 10, respectivamente, a um trabalho de laborátorio, 
+# a uma avaliação semestral e um exame final. A média das três notas mencionadas anteriormente obedece aos pesos: Trabalho de Laboratório: 2; Avaliação Semestral: 3; 
+# Exame Final: 5. De acordo com o resultado, mostre na tela se o aluno está aprovado (média entre 0 e 2,9) de recuperação (entre 3 e 4,9) ou se foi aprovado.
+# Faça todas as verificações necessárias.
 
 notaLab = float(input('Nota trabalho laboratorio '))
 notaAval = float(input('Nota avaliação semestral '))
